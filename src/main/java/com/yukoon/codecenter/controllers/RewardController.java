7e1558fba4efb4cc18c92449153df70375ffae43
a5928497 <61722615@qq.com>
2018-07-12 17:00:11 +0800

    developing

--- src/main/java/com/yukoon/codecenter/controllers/RewardController.java
+++ src/main/java/com/yukoon/codecenter/controllers/RewardController.java
@@ -1,12 +1,14 @@
 package com.yukoon.codecenter.controllers;
 
 import com.yukoon.codecenter.entities.Page;
+import com.yukoon.codecenter.entities.Reward;
 import com.yukoon.codecenter.services.RewardService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -29,5 +31,22 @@ public class RewardController {
         Page page = rewardService.findAll(pageNo,PAGE_SIZE);
         map.put("page",page);
         return "backend/all_reward_list";
+    }
+
+    //后台前往添加礼品
+    @RequiresRoles("admin")
+    @RequiresPermissions("add")
+    @GetMapping("/reward")
+    public String toAdd() {
+        return "backend/reward_input";
+    }
+
+    //后台添加礼品
+    @RequiresRoles("admin")
+    @RequiresPermissions("add")
+    @PostMapping("/reward")
+    public String Add(Reward reward) {
+        rewardService.addReward(reward);
+        return "redirect:/rewards";
     }
 }
