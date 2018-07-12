32eba56def928f831d0bb32f84b672c3f9e5beaf
a5928497 <61722615@qq.com>
2018-07-12 19:00:47 +0800

    developing

--- src/main/java/com/yukoon/codecenter/entities/Code.java
+++ src/main/java/com/yukoon/codecenter/entities/Code.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -15,4 +17,5 @@ public class Code {
     private Integer status;
     private Integer reward_id;
     private Integer record_id;
+    private Date expiration_date;
 }
--- src/main/java/com/yukoon/codecenter/providers/CodeMapperProvider.java
+++ src/main/java/com/yukoon/codecenter/providers/CodeMapperProvider.java
@@ -3,19 +3,16 @@ package com.yukoon.codecenter.providers;
 import com.yukoon.codecenter.entities.Code;
 
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CodeMapperProvider {
 
     public String insertAll(Map map) {
         List<Code> codes = (List<Code>) map.get("list");
         StringBuffer sb = new StringBuffer();
-        sb.append("INSERT INTO codes (code,status,reward_id,record_id) VALUES");
+        sb.append("INSERT INTO codes (code,status,reward_id,record_id,expiration_date) VALUES");
         MessageFormat mf = new MessageFormat("#'{'list[{0}].code'}',#'{'list[{0}].status'}',#'{'list[{0}].reward_id'}'," +
-                "#'{'list[{0}].record_id'}'");
+                "#'{'list[{0}].record_id'}',#'{'list[{0}].expiration_date'}'");
         for (int i = 0;i< codes.size();i++) {
             sb.append("(");
             //这里如果直接用数字，超过1000会格式化变成1,000，但是String类型就没问题
@@ -30,8 +27,8 @@ public class CodeMapperProvider {
     }
 
     public static void main(String[] args) {
-        Code code1 = new Code().setCode("123123").setStatus(1).setReward_id(1).setRecord_id(1);
-        Code code2 = new Code().setCode("sdfdsfd").setStatus(1).setReward_id(1).setRecord_id(1);
+        Code code1 = new Code().setCode("123123").setStatus(1).setReward_id(1).setRecord_id(1).setExpiration_date(new Date());
+        Code code2 = new Code().setCode("sdfdsfd").setStatus(1).setReward_id(1).setRecord_id(1).setExpiration_date(new Date());
         List<Code> list = new ArrayList<>();
         list.add(code1);
         list.add(code2);
